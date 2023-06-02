81e0f676ea85744180ea7e053b8341cb71a780b2
Xeokj <jahysu7300@naver.com>
2023-06-03 02:02:52 +0900

    230603

---
+++ 2556.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main() {
+	int N, i, j;
+	scanf("%d", &N);
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}
