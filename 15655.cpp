8e896a370058cb8465be864cd3c3735f17e125be
Xeokj <jahysu7300@naver.com>
2023-07-21 18:10:11 +0900

    230718 - 예비군 대신

---
+++ 15655.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int arr[9], ans[9];
+void nm(int num, int cur);
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++)
+		cin >> arr[i];
+	sort(arr, arr + N + 1);
+	nm(1, 1);
+}
+
+void nm(int num, int cur) {
+	if (num > M) {
+		for (int i = 1; i <= M; i++)
+			cout << ans[i] << ' ';
+		cout << "\n";
+		return;
+	}
+	for (int i = cur; i <= N; i++) {
+		ans[num] = arr[i];
+		nm(num + 1, i + 1);
+	}
+}
